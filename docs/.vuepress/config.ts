d7af1e6d635f38fd7b971e23981c434d12981cf2
郑薛林 <zhengxuelin@yhbj.cn>
2024-09-01 18:36:51 +0800

    fix:VuePress 已经不支持动态导入主题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,36 +1,36 @@
-var sidebar = require('./sidebar')
-var nav = require('./nav')
+var sidebar = require("./sidebar")
+var nav = require("./nav")
 
+import { hopeTheme } from "vuepress-theme-hope"
 
-import { hopeTheme } from "vuepress-theme-hope";
-
-const { defaultTheme } = require('@vuepress/theme-default')
+// const { defaultTheme } = require('@vuepress/theme-default')
+import { defaultTheme } from "@vuepress/theme-default"
 module.exports = {
-    title: '逆水行舟丨',
-    // frontmatter: true,
-    description: '真正的大师, 永远有一颗学徒的心',
-    head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
-    ],
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': './public',
-            },
-        },
+  title: "逆水行舟丨",
+  // frontmatter: true,
+  description: "真正的大师, 永远有一颗学徒的心",
+  head: [
+    ["link", { rel: "icon", href: "/favicon.ico" }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": "./public",
+      },
     },
-    markdown: {
-        lineNumbers: true,
-    },
-    theme: defaultTheme({
-        // 默认主题配置
-        navbar: nav,
-        sidebar: sidebar,
-        // plugins: {
-        //     comment: {
-        //         type: "waline",
-        //         serverURL: "...", // your serverURL
-        //     },
-        // },
-    })
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: nav,
+    sidebar: sidebar,
+    // plugins: {
+    //     comment: {
+    //         type: "waline",
+    //         serverURL: "...", // your serverURL
+    //     },
+    // },
+  }),
 }
