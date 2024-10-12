f605537532caa97a784249eda8b07d6441a89163
zxl7 <zhengxuelin@yhbj.cn>
2024-10-12 15:51:07 +0800

    feat: vue-router 源码解析

--- blog/.vuepress/navbar.ts
+++ blog/.vuepress/navbar.ts
@@ -1,3 +1,8 @@
 import { navbar } from "vuepress-theme-hope"
 
-export default navbar(["/", { text: "掘金", link: "https://juejin.cn/user/3624450346452542" }, { text: "GitHub", link: "https://github.com/zxl7" }])
+export default navbar([
+  "/",
+  { text: "掘金", link: "https://juejin.cn/user/3624450346452542" },
+  { text: "GitHub", link: "https://github.com/zxl7" },
+  { text: "低代码设计", link: "/" },
+])
